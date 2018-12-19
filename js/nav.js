5edca435ad8ac8209096cad6c12689f1ee801d47
corentin <corentin.guillard@ynov.com>
2018-12-19 12:04:35 +0100

    push d'un projet local sur mon github

---
+++ js/nav.js
@@ -0,0 +1,17 @@
+function linkOne() {
+    document.getElementById("navLinkOne").className = "actif";
+    document.getElementById("navLinkTwo").className -= "actif";
+    document.getElementById("navLinkThree").className -= "actif";
+}
+
+function linkTwo() {
+    document.getElementById("navLinkOne").className -= "actif";
+    document.getElementById("navLinkTwo").className = "actif";
+    document.getElementById("navLinkThree").className -= "actif";
+}
+
+function linkThree() {
+    document.getElementById("navLinkOne").className -= "actif";
+    document.getElementById("navLinkTwo").className -= "actif";
+    document.getElementById("navLinkThree").className = "actif";
+}
